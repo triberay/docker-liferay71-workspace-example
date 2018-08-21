0709a216e7bc7cff7797f08a126754d489768d6b
Wouter Vernaillen <wouter@vernaillen.com>
2018-08-21 18:40:18 +0200

    selenium browser test

---
+++ modules/triberay-docker-info-portlet/src/testIntegration/java/com/triberay/docker/info/test/SimpleTest.java
@@ -0,0 +1,48 @@
+package com.triberay.docker.info.test;
+
+
+import junit.framework.TestCase;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+@RunWith(JUnit4.class)
+public class SimpleTest extends TestCase {
+
+    private WebDriver driver;
+
+    @Before
+    public void setUp() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("firefox");
+
+        driver = new RemoteWebDriver(URI.create("http://127.0.0.1:4444/wd/hub").toURL(), capabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testSimple() {
+        this.driver.get("http://httpd");
+
+        String body = this.driver.findElement(By.tagName("body")).getText();
+
+        Assert.assertEquals("Triberay Docker Example Setup - Liferay", this.driver.getTitle());
+        Assert.assertTrue("The portlet is not well deployed", body.contains("Hello from Triberay Docker Info!"));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        this.driver.quit();
+    }
+}
